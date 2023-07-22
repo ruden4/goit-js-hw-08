bcb1192604eb09a1936b2081ba3c98d42e3d4544
ruden4 <ruden4@gmail.com>
2023-07-22 20:14:58 +0300

    Second Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+// Достаем время видео и получаем его из localStorage//
+player.on('timeupdate', throttle((event) => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}, 1000));
+
+// Получаем время из localStorage как продолжение воспроизведения//
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
 
